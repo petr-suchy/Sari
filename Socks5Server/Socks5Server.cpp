7b079662b30f9b95ef798d76bb53b9a13dff68ee
Petr Suchý <petrs.sokolo@seznam.cz>
2024-06-26 11:38:42 +0200

    Added Socks5Server project.

---
+++ Socks5Server/Socks5Server.cpp
@@ -0,0 +1,10 @@
+// Socks5Server.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "Promise.h"
+
+int main()
+{
+    return 0;
+}
