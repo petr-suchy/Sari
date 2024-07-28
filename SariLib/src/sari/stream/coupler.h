c53634f0fc5098ea780eae82705f513dbb79c4c8
Petr Suchý <petrs.sokolo@seznam.cz>
2024-07-28 19:41:27 +0200

    Added Utils::Coupler class.

---
+++ SariLib/src/sari/stream/coupler.h
@@ -0,0 +1,57 @@
+#pragma once
+
+namespace Sari { namespace Stream {
+
+	template<typename ReadableEnd, typename WritableEnd = ReadableEnd>
+	class Coupler {
+	public:
+
+		using executor_type = typename ReadableEnd::executor_type;
+
+		Coupler(boost::asio::io_context& ioContext) :
+			readableEnd_(ioContext),
+			writableEnd_(ioContext)
+		{}
+
+		Coupler(boost::asio::any_io_executor ioExecutor) :
+			readableEnd_(ioExecutor),
+			writableEnd_(ioExecutor)
+		{}
+
+		boost::asio::any_io_executor get_executor()
+		{	return readableEnd_.get_executor();
+		};
+
+		WritableEnd& readable_end() { return writableEnd_; }
+		ReadableEnd& writable_end() { return readableEnd_; }
+
+		template<typename ConstBufferSequence, typename WriteHandler>
+		void async_write_some(
+			const ConstBufferSequence& buffers,
+			WriteHandler handler
+		) {
+			writableEnd_.async_write_some(buffers, handler);
+		}
+
+		template<typename MutableBufferSequence, typename ReadHandler>
+		void async_read_some(
+			const MutableBufferSequence& buffers,
+			ReadHandler&& handler
+		) {
+			readableEnd_.async_read_some(buffers, handler);
+		}
+
+		void close()
+		{
+			readableEnd_.close();
+			writableEnd_.close();
+		}
+
+	private:
+
+		ReadableEnd readableEnd_;
+		WritableEnd writableEnd_;
+
+	};
+
+}}
---
+++ examples/Coupler/Coupler.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include "sari/asio/asio.h"
+#include "sari/stream/coupler.h"
+
+namespace asio = boost::asio;
+namespace Asio = Sari::Asio;
+namespace Utils = Sari::Utils;
+namespace Stream = Sari::Stream;
+
+template<typename Stream, typename StreamBuf>
+static Utils::Promise AsyncReadLine(std::shared_ptr<Stream> stream, std::shared_ptr<StreamBuf> streamBuf)
+{
+    return Asio::AsyncReadUntil(*stream, *streamBuf, "\n")
+        .then([stream, streamBuf]() {
+
+            // Extract one line from the stream buffer.
+            std::istream is(streamBuf.get());
+            std::string line;
+            std::getline(is, line);
+
+            return Utils::Promise::Resolve(stream->get_executor(), line, streamBuf);
+        });
+}
+
+template<typename Stream>
+static Utils::Promise AsyncEcho(std::shared_ptr<Stream> stream)
+{
+    auto streamBuf = std::make_shared<asio::streambuf>();
+
+    auto task = [stream, streamBuf]() {
+        return AsyncReadLine(stream, streamBuf)
+            .then([stream](std::string line) {
+                auto resp = std::make_shared<std::string>(line + '\n');
+                return Asio::AsyncWriteSome(*stream, asio::buffer(*resp))
+                    .then([stream, resp]() {
+                        return true;
+                    });
+            });
+    };
+
+    return Utils::Promise::Repeat(stream->get_executor(), task);
+}
+
+template<typename Stream>
+static Utils::Promise AsyncClient(std::shared_ptr<Stream> stream)
+{
+    auto streamBuf = std::make_shared<asio::streambuf>();
+
+    return Utils::Promise::Resolve(stream->get_executor())
+        .then([stream, streamBuf]() {
+            auto msg = std::make_shared<std::string>("Hello!\n");
+            return Asio::AsyncWriteSome(*stream, asio::buffer(*msg))
+                .then([stream, streamBuf, msg]() {
+                    return AsyncReadLine(stream, streamBuf);
+                });
+        }).then([stream, streamBuf](std::string resp) {
+
+            std::cout << resp << '\n';
+
+            auto msg = std::make_shared<std::string>("Good bye.\n");
+
+            return Asio::AsyncWriteSome(*stream, asio::buffer(*msg))
+                .then([stream, streamBuf, msg]() {
+                    return AsyncReadLine(stream, streamBuf);
+                });
+        }).then([stream](std::string resp) {
+            std::cout << resp << '\n';
+            stream->close();
+        });
+}
+
+int main()
+{
+    using PipeCoupler = Stream::Coupler<boost::asio::readable_pipe, boost::asio::writable_pipe>;
+
+    asio::io_context ioContext;
+
+    auto sourcePipe = std::make_shared<PipeCoupler>(ioContext);
+    auto sinkPipe = std::make_shared<PipeCoupler>(ioContext);
+
+    boost::asio::connect_pipe(sourcePipe->writable_end(), sinkPipe->readable_end());
+    boost::asio::connect_pipe(sinkPipe->writable_end(), sourcePipe->readable_end());
+
+    Utils::Promise echo = AsyncEcho(sourcePipe);
+    Utils::Promise client = AsyncClient(sinkPipe);
+
+    Utils::Promise::AllSettled(ioContext, { echo, client })
+        .then([]() {
+            std::cout << "End\n";
+        });
+
+    ioContext.run();
+
+    return 0;
+}
