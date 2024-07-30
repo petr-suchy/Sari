20adc7cf206e9685edf22074d089ef6335a49608
Petr Suchý <petrs.sokolo@seznam.cz>
2024-07-30 17:49:19 +0200

    Extended Proxy example.

--- examples/Proxy/Main.cpp
+++ examples/Proxy/Main.cpp
@@ -14,20 +14,24 @@ int main(int argc, char* argv[])
 
     try {
 
+        Utils::Exchanger exchanger;
+
         Net::Server server(
             ioContext, 1234,
-            [](const boost::system::error_code& ec, boost::asio::ip::tcp::socket peer) {
+            [&exchanger](const boost::system::error_code& ec, boost::asio::ip::tcp::socket peer) {
 
                 if (ec) {
                     std::cerr << ec.category().name() << " error: " << ec.message() << '\n';
                     return;
                 }
 
-                StartProxy(std::move(peer))
+                Proxy::Start(std::move(peer), exchanger)
                     .fail([](const boost::system::error_code ec) {
                         std::cerr << ec.category().name() << " error: " << ec.message() << '\n';
                     }).fail([](const std::exception e) {
                         std::cerr << "error: " << e.what() << '\n';
+                    }).fail([](std::string what) {
+                        std::cerr << "error: " << what << '\n';
                     }).fail([]() {
                         std::cerr << "an unknown error occurred\n";
                     });
--- examples/Proxy/Proxy.h
+++ examples/Proxy/Proxy.h
@@ -2,81 +2,165 @@
 
 #include "sari/asio/asio.h"
 #include "sari/string/trim.h"
+#include "sari/utils/exchanger.h"
+#include "Sari/stream/coupler.h"
+#include "sari/stream/forward.h"
 
-template<typename Socket>
-Sari::Utils::Promise StartProxy(Socket&& peer)
-{
-    namespace asio = boost::asio;
-	namespace Asio = Sari::Asio;
-	namespace Utils = Sari::Utils;
-	namespace String = Sari::String;
+class Proxy {
+public:
 
-	auto sock = std::make_shared<boost::asio::ip::tcp::socket>(std::move(peer));
-    auto streamBuf = std::make_shared<asio::streambuf>();
+    template<typename Socket>
+    static Sari::Utils::Promise Start(Socket&& peer, Sari::Utils::Exchanger& exchanger)
+    {
+        namespace asio = boost::asio;
+        namespace Asio = Sari::Asio;
+        namespace Utils = Sari::Utils;
+        namespace Stream = Sari::Stream;
 
-    return Utils::Promise::Resolve(sock->get_executor())
-        .then([sock, streamBuf]() {
-        
-            auto readUntilCommandTask = [sock](std::shared_ptr<asio::streambuf> streamBuf) {
-                return Asio::AsyncReadUntil(*sock, *streamBuf, "\r\n")
-                    .then([sock, streamBuf]() {
+	    auto sock = std::make_shared<boost::asio::ip::tcp::socket>(std::move(peer));
 
-                        // Extract one line from the stream buffer.
-                        std::istream is(streamBuf.get());
-                        std::string line;
-                        std::getline(is, line);
-
-                        std::string command = String::Trim(line).str();
-
-                        if (command.empty()) {
-                            // When the line is empty, read again.
-                            return Utils::Promise::Resolve(sock->get_executor(), true, streamBuf);
+        return Utils::Promise::Resolve(sock->get_executor())
+            .then([sock]() {
+                // Try to receive a command in the time limit.
+                return Utils::Promise::AllSettled(sock->get_executor(),
+                        { Asio::AsyncDeadline(AsyncReadUntilCommand(sock), boost::asio::chrono::seconds(15)) }
+                    ).then([sock](Utils::Promise p) {
+                        if (p.isFulfilled()) {
+                            return Utils::Promise::Resolve(p.getExecutor(), p.result());
                         }
-                        else {
-                            // Otherwise resolve with the command string.
-                            return Utils::Promise::Resolve(sock->get_executor(), false, command, streamBuf);
+                        else { // Rejected
+                            return Asio::AsyncWrite(*sock, "ERR " + PromiseResultToStatusTest(p) + "\r\n")
+                                .then([sock, p]() {
+                                    sock->shutdown(boost::asio::ip::tcp::socket::shutdown_both);
+                                    return Utils::Promise::Reject(p.getExecutor(), p.result());
+                                });
                         }
-                });
-            };
+                    });
+            }).then([sock, &exchanger](std::string command) {
 
-            // Repeat the command reading task until a command is received.
-            Utils::Promise readUntilCommand = Utils::Promise::Repeat(
-                sock->get_executor(),
-                readUntilCommandTask, streamBuf
-            );
+                using PipeCoupler = Stream::Coupler<boost::asio::readable_pipe, boost::asio::writable_pipe>;
 
-            // Try to receive a command in the time limit.
-            return Utils::Promise::AllSettled(sock->get_executor(), { Asio::AsyncDeadline(readUntilCommand, boost::asio::chrono::seconds(15)) })
-                .then([sock](Utils::Promise p) {
-                    if (p.state() == Utils::Promise::State::Fulfilled) {
-                        return Utils::Promise::Resolve(p.getExecutor(), p.result());
+                auto sourcePipe = std::make_shared<PipeCoupler>(sock->get_executor());
+                auto sinkPipe = std::make_shared<PipeCoupler>(sock->get_executor());
+
+                boost::asio::connect_pipe(sourcePipe->writable_end(), sinkPipe->readable_end());
+                boost::asio::connect_pipe(sinkPipe->writable_end(), sourcePipe->readable_end());
+
+                auto trans = std::make_shared<Utils::Exchanger::Transaction>(sock->get_executor());
+
+                Utils::Promise sinkToOther;
+
+                if (command == "CONNECT") {
+                    sinkToOther = exchanger.asyncConsume(*trans, sinkPipe)
+                        .then([sinkPipe, command](std::shared_ptr<PipeCoupler> other) {
+                            return Asio::AsyncWrite(*other, "ACK\r\n")
+                                .then([sinkPipe, other, command]() {
+                                    std::cout << command << " request forwarded.\n";
+                                    return Stream::Forward(*sinkPipe, *other)
+                                        .then([sinkPipe, other]() {});
+                                });
+                        });
+                }
+                else if (command == "BIND") {
+                    sinkToOther = exchanger.asyncPrtoduce(*trans, sinkPipe)
+                        .then([sinkPipe, command](std::shared_ptr<PipeCoupler> other) {
+                            std::cout << command << " request forwarded.\n";
+                            return Stream::Forward(*sinkPipe, *other)
+                                .then([sinkPipe, other]() {});
+                        });
+                }
+                else {
+                    return Asio::AsyncWrite(*sock, "ERR Invalid command\r\n")
+                        .then([sock]() {
+                            sock->shutdown(boost::asio::ip::tcp::socket::shutdown_both);
+                            return Utils::Promise::Reject(sock->get_executor(), std::string{"invalid command"});
+                        });
+                }
+
+                if (trans->isPending()) {
+                    std::cout << command << " request is pending...\n";
+                }
+
+                auto sockToSource = Stream::Forward(*sock, *sourcePipe)
+                    .then([sock, sourcePipe, trans, command]() {
+
+                        bool isPending = trans->isPending();
+
+                        trans->cancel();
+
+                        if (isPending && !trans->isPending()) {
+                            std::cout << command << " request canceled.\n";
+                        }
+                    });
+
+                return Utils::Promise::All(sock->get_executor(), { sockToSource, sinkToOther });
+            }).finalize([sock](Utils::Promise p) {
+                sock->close();
+            });
+    }
+
+private:
+
+    static Sari::Utils::Promise AsyncReadUntilCommand(std::shared_ptr<boost::asio::ip::tcp::socket> sock)
+    {
+        namespace asio = boost::asio;
+        namespace Asio = Sari::Asio;
+        namespace Utils = Sari::Utils;
+        namespace String = Sari::String;
+
+        auto streamBuf = std::make_shared<asio::streambuf>();
+
+        auto readUntilCommandTask = [sock](std::shared_ptr<asio::streambuf> streamBuf) {
+            return Asio::AsyncReadUntil(*sock, *streamBuf, "\r\n")
+                .then([sock, streamBuf]() {
+
+                    // Extract one line from the stream buffer.
+                    std::istream is(streamBuf.get());
+                    std::string line;
+                    std::getline(is, line);
+
+                    std::string command = String::Trim(line).str();
+
+                    if (command.empty()) {
+                        // When the line is empty, read again.
+                        return Utils::Promise::Resolve(sock->get_executor(), true, streamBuf);
                     }
-                    else { // Rejected
-
-                        auto errorMessage = std::make_shared<std::string>("ERR Internal server error.\r\n");
-
-                        if (!p.result().empty() && p.result()[0].type() == typeid(boost::system::error_code)) {
-
-                            auto ec = std::any_cast<boost::system::error_code>(p.result()[0]);
-
-                            if (ec == make_error_code(boost::system::errc::timed_out)) {
-                                *errorMessage = "ERR Command timeout.\r\n";
-                            }
-                        }
-
-                        return Asio::AsyncWriteSome(*sock, asio::buffer(*errorMessage))
-                            .then([sock, p, errorMessage]() {
-                                sock->shutdown(boost::asio::ip::tcp::socket::shutdown_both);
-                                return Utils::Promise::Reject(p.getExecutor(), p.result());
-                            });
+                    else {
+                        // Otherwise resolve with the command string.
+                        return Utils::Promise::Resolve(sock->get_executor(), false, command, streamBuf);
                     }
-                });
-        }).then([sock](std::string command) {
+            });
+        };
 
-            std::cout << ": \"" << command << "\"\n";
-            sock->shutdown(boost::asio::ip::tcp::socket::shutdown_both);
+        // Repeat the command reading task until a command is received.
+        return Utils::Promise::Repeat(sock->get_executor(), readUntilCommandTask, streamBuf);
+    }
 
-        }).finalize([sock](Utils::Promise p) {
-            sock->close();
-        });
-}
+    static std::string PromiseResultToStatusTest(Sari::Utils::Promise p)
+    {
+        if (p.isPending()) {
+            return "Pending";
+        }
+        else if (p.isFulfilled()) {
+            return "Successful";
+        }
+
+        // When rejected:
+
+        std::string statusText = "Internal server error";
+
+        if (p.result().empty() || p.result(0).type() != typeid(boost::system::error_code)) {
+            return statusText;
+        }
+
+        auto ec = p.result<boost::system::error_code>(0);
+
+        if (ec == make_error_code(boost::system::errc::timed_out)) {
+            statusText = "Timeout";
+        }
+
+        return statusText;
+    }
+
+};
+
